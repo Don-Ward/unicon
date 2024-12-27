76a65e8317c365364752580eb4d124ffa415e39d
Don Ward <don@careful.co.uk>
2024-12-27 22:24:07 +0000

    Add registers to the set that are saved on co-expression switch.
    The recommended save set in the System V ABI is
        RBX, RBP, R12-R15
    So, to be on the safe side, all of those are saved (even if they aren't used).

--- src/asm/x86_64_macos-rswitch.s
+++ src/asm/x86_64_macos-rswitch.s
@@ -21,11 +21,22 @@ _coswitch:
 
         movq    %rsp, 0(%rdi)      # Old stack pointer -> old_cstate[0]
         movq    %rbp, 8(%rdi)      # Old base pointer -> old_cstate[1]
+        movq    %rbx, 16(%rdi)     # Old bx -> old_cstate[2]
+        movq    %r12, 24(%rdi)     # Old r12 -> old_cstate[3]
+        movq    %r13, 32(%rdi)     # Old r13 -> old_cstate[4]
+        movq    %r14, 40(%rdi)     # Old r14 -> old_cstate[5]
+        movq    %r15, 48(%rdi)     # Old r15 -> old_cstate[6]
+        #
         movq    0(%rsi), %rsp      # new_cstate[0] -> new stack pointer
         movq    8(%rsi), %rbp      # new_cstate[1] -> new base pointer
+        movq    16(%rsi), %rbx     # new_cstate[2] -> new bx
         orl     %edx, %edx         # Is this the first activation?
-        je      .L1                # If so, skip.
-        ret                        # Otherwise we are done.
+        je      .L1                # If so, skip the rest and call new_context
+        movq    24(%rsi), %r12     # new_cstate[3] -> new r12
+        movq    32(%rsi), %r13     # new_cstate[4] -> new r13
+        movq    40(%rsi), %r14     # new_cstate[5] -> new r14
+        movq    48(%rsi), %r15     # new_cstate[6] -> new r15
+        ret     # to new coexpression
 .L1:    xorl    %edi, %edi         # Call new_context((int) 0, (ptr) 0)
         xorl    %esi, %esi         # (Implicitly zero-extended to 64 bits)
         callq    _new_context
